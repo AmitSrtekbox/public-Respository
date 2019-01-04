c4450a7aaf38ea2014494c8a784cc9cedcc859d2
srtekMAC <srtekmac@srtekmacs-macbook-air.local>
2019-01-04 12:45:57 +0530

    Initial Commit

---
+++ StackViews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StackViews
+//
+//  Created by srtekMAC on 04/01/19.
+//  Copyright © 2019 srtekMAC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
